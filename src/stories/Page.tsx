a2f6d3e6e16b6aaa4fadb9846651b97984efe213
James Burdge <jamesburdge@Jamess-MacBook-Pro.local>
2022-09-09 11:48:40 +0900

    inital commit

---
+++ src/stories/Page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Page: React.FC = () => {
+
+  return (
+    <article>
+      <h1>Example Page</h1>
+      <button>Click to make API request</button>
+    </article>
+  );
+};
